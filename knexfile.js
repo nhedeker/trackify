6d85c3e67ecb4b5679290f3839a54035c0c259c0
Natasha Hedeker <Natasha.Hedeker@gmail.com>
2016-06-27 15:57:41 -0700

    Fixed error for successful Heroku build

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/trackify_dev'
-  }
+  },
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
