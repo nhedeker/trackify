1c4fae03ceb2ea5cf072b7b9bb4d638250778175
Natasha Hedeker <Natasha.Hedeker@gmail.com>
2016-06-28 15:08:24 -0700

    Created our first database-driven route handler

--- routes/artists.js
+++ routes/artists.js
@@ -2,5 +2,32 @@
 
 const express = require('express');
 const router = express.Router();
+const knex = require('../knex');
+
+router.get('/artists', (_req, res, next) => {
+  knex('artists')
+    .orderBy('id')
+    .then((artists) => {
+      res.send(artists);
+    })
+    .catch((err) => {
+      next(err);
+    })
+});
+
+router.get('/artsists/:id', (req, res, next) => {
+  knex('artists')
+    .where('id', req.params.id)
+    .first()
+    .then((artist) => {
+      if (!artist) {
+        return next();
+      }
+      res.send(artist);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
