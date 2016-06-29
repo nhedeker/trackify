28ff6a6ef1ac3bac96a0bfed9f964bd64c337bdd
Natasha Hedeker <Natasha.Hedeker@gmail.com>
2016-06-29 13:32:24 -0700

    Used bcrypt to hash passwords

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,21 @@
 
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 router.post('/users', (req, res, next) => {
-  res.sendStatus(200);
+  // 1st arg: what we want to hash
+  // 2nd arg: work factor
+  // 3rd arg: callback
+  // 1st arg call: error if error
+  // 2nd arg call: the resulting hashed thing
+  bcrypt.hash(req.body.password, 12, (err, hashed_password) => {
+    if (err) {
+      return next(err);
+    }
+    console.log(req.body.email, hashed_password);
+    res.sendStatus(200);
+  });
 });
 
 module.exports = router;
